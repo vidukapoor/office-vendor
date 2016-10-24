3629061f46fdd2c4d82a71608d2c79d03f8cfcb4
vidukapoor <vishwadeepkapoor@gmail.com>
2016-10-24 18:25:38 +0530

    sdvdfbvdfb

--- imports/startup/server/config.js
+++
@@ -1,8 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: "951228542205-k1rah7g4drr54q56oavf0re2c0npggnk.apps.googleusercontent.com",
-  secret: "RoNv2GFApnw5OR2CE25FGyLH"
-});
